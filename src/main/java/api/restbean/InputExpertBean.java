74e31dfbbe39850ae239b528a64fc394c2ade4c3
benevma <46048328+benevma@users.noreply.github.com>
2020-11-18 16:40:07 +0100

    Update InputExpertBean.java

--- src/main/java/api/restbean/InputExpertBean.java
+++ src/main/java/api/restbean/InputExpertBean.java
@@ -6,26 +6,12 @@ public class InputExpertBean  implements java.io.Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	boolean update;
-	String bearer;
 	InputBean[] documents;
 	
-	public boolean isUpdate() {
-		return update;
-	}
-	public void setUpdate(boolean update) {
-		this.update = update;
-	}
 	public InputBean[] getDocuments() {
 		return documents;
 	}
 	public void setDocuments(InputBean[] documents) {
 		this.documents = documents;
-	}
-	public String getBearer() {
-		return bearer;
-	}
-	public void setBearer(String bearer) {
-		this.bearer = bearer;
 	}
 }	
