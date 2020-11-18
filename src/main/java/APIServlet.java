5cfa58c109003dfbe5522fa5dd59330f8c80d16e
benevma <46048328+benevma@users.noreply.github.com>
2020-11-18 16:41:58 +0100

    Update APIServlet.java

--- src/main/java/APIServlet.java
+++ src/main/java/APIServlet.java
@@ -42,8 +42,6 @@ public class APIServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		InputExpertBean in = new InputExpertBean();
-		in.setBearer(request.getParameter("bearer"));
-		in.setUpdate(request.getParameter("update")!=null);
 
         //Get all doc-i input file contents
         ArrayList<InputBean> array = new ArrayList<InputBean>();
